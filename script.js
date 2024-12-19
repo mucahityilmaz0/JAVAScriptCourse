e729778381e83aa3199ece6ac9ad9a717eb88f6d
mucahityilmaz <390925@ogr.ktu.edu.tr>
2024-12-19 15:18:54 +0300

    Values and Variables

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
+//if (js === "amazing") alert("JavaScript is FUN!");
 console.log(1 + 1 + 1 + 1);
+
+let firstName = "Mücahit";
+console.log(firstName);
+let $function = 27; //$ for token as a variable
+console.log($function);
+
+let PI = 3.14;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Student";
+
+console.log(myFirstJob, myCurrentJob);
+
+let country = "Turkey";
+let continent = "Asia";
+let population = 1;
+
+console.log(country);
+console.log(continent);
+console.log(population);
