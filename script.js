31af66348de88f98fb011d2b81adf4718c399b91
mucahityilmaz <390925@ogr.ktu.edu.tr>
2024-12-25 16:33:10 +0300

    Truthy and Falsy Values

--- script.js
+++ script.js
@@ -131,7 +131,6 @@ if (birthYear <= 2000) {
 }
 console.log(century);
 
-*/
 
 const inputYear = "1996";
 
@@ -152,3 +151,28 @@ n--;
 console.log(n);
 n = n - 1;
 console.log(n);
+
+*/
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Mücahit"));
+console.log(Boolean({}));
+console.log(typeof {});
+
+console.log(Boolean(""));
+
+const money = 0;
+
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
