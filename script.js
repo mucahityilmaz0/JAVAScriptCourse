7d31f739ef7dd11216357be6213018a2b9d0ded7
mucahityilmaz <390925@ogr.ktu.edu.tr>
2025-01-09 23:58:26 +0300

    Reviewing Functions

--- script.js
+++ script.js
@@ -321,18 +321,38 @@ const yearUntilRetirement = (birthYear, firstName) => {
 console.log(yearUntilRetirement(1996, "Mücahit"));
 console.log(yearUntilRetirement(1994, "Mert"));
 
-*/
 
 function cutFruitPieces(fruit) {
-  return fruit * 4;
+    return fruit * 4;
 }
 
 function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
-
-  const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
   return juice;
 }
 
 console.log(fruitProcessor(2, 3));
+
+*/
+const calcAge = function (birthYear) {
+  return 2025 - birthYear;
+};
+
+const yearUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired`);
+    return -1;
+  }
+};
+
+console.log(yearUntilRetirement(1996, "Mücahit"));
+console.log(yearUntilRetirement(1960, "Mahmut"));
